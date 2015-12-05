027d120c9ea3f7b93d44b55dbe2d4f8b8dcec4e1
Dhruv Karan <k4r4n.dhruv@gmail.com>
2015-12-05 19:18:59 +0530

    Pre commit for task 2. TO-ADD: Write output to CSV

---
+++ code_task2.py
@@ -0,0 +1,66 @@
+import csv
+import numpy as np 
+from sklearn.preprocessing import Imputer
+from sklearn import linear_model
+import pandas as pd
+
+def train():
+
+	with open('products.csv','rb') as f:
+			firstLine = False
+			mycsv = csv.reader(f)
+			ls = []
+			ls1 = []
+			ls2 = []
+			ls4_1 = []
+			ls4_2 = []
+			for row in mycsv:
+				if firstLine == False:
+					firstLine = True
+				else:
+					p1 = row[5]
+					p2 = row[6]
+					p4 = row[8]
+					if len(p1) == 0:
+						continue
+					else:
+						p1=int(p1)
+						p4_1=int(p4)
+						ls1.append(p1)
+						ls4_1.append(p4_1)
+					if len(p2) == 0:
+						continue
+					else:
+						p2=int(p2)
+						p4_2=int(p4)
+						ls2.append(p2)
+						ls4_2.append(p4_2)
+						
+			d1 = pd.DataFrame(ls1)
+			d4_1 = pd.DataFrame(ls4_1)
+			d2 = pd.DataFrame(ls2)
+			d4_2 = pd.DataFrame(ls4_2)
+			regr1 = linear_model.LinearRegression()
+	 		regr1.fit(d4_1, d2)
+	 		regr2 = linear_model.LinearRegression()
+	 		regr2.fit(d4_2, d2)
+ 		
+ 	return regr1, regr2
+def predict():
+	regr1, regr2 = train()
+	with open('products.csv','rb') as f:
+		firstLine = False
+		mycsv = csv.reader(f)
+		for row in mycsv:
+				if firstLine == False:
+					firstLine = True
+				else:
+					p1 = row[5]
+					p2 = row[6]
+					p4 = int(row[8])
+					if len(p1) == 0:
+						predictedValue = regr1.predict(p4)
+					if len(p2) == 0:
+						predictedValue = regr2.predict(p4)
+
+predict()
